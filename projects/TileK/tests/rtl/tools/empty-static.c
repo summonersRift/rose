e56896e8f77fcc0213dc7d28bd8471e5bb7e4897
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2015-10-15 15:20:42 -0400

    [KLT] Dump of OpenCL is working.

---
+++ projects/TileK/tests/rtl/tools/empty-static.c
@@ -0,0 +1,11 @@
+
+#include "KLT/RTL/kernel.h"
+
+char * opencl_kernel_file = NULL;
+char * opencl_kernel_options = NULL;
+char * opencl_klt_runtime_lib = NULL;
+
+char * klt_file_stem = NULL;
+size_t klt_num_kernels = 0;
+struct klt_kernel_desc_t klt_kernel_desc[0];
+
--- src/midend/KLT/lib/rtl/opencl-utils.c
+++ src/midend/KLT/lib/rtl/opencl-utils.c
@@ -7,9 +7,220 @@
 
 #include <assert.h>
 
+enum klt_opencl_property_type_e {
+  e_klt_opencl_property_is_cl_bool,
+  e_klt_opencl_property_is_cl_uint,
+  e_klt_opencl_property_is_cl_ulong,
+  e_klt_opencl_property_is_size_t,
+  e_klt_opencl_property_is_array_size_t,
+  e_klt_opencl_property_is_array_char,
+  e_klt_opencl_property_is_array_char_split,
+  e_klt_opencl_property_is_cl_device_type,
+  e_klt_opencl_property_is_cl_device_local_mem_type,
+  e_klt_opencl_property_is_cl_device_mem_cache_type,
+  e_klt_opencl_property_is_cl_device_exec_capabilities,
+  e_klt_opencl_property_is_cl_command_queue_properties,
+  e_klt_opencl_property_is_cl_device_fp_config
+};
+
+struct klt_opencl_property_t {
+   char * name;
+   cl_device_info property;
+   enum klt_opencl_property_type_e type;
+};
+
+#define KLT_OPENCL_NUM_PROPERTIES 51
+
+struct klt_opencl_property_t klt_opencl_property[KLT_OPENCL_NUM_PROPERTIES] = {
+  { "CL_DEVICE_ADDRESS_BITS" , CL_DEVICE_ADDRESS_BITS , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_AVAILABLE" , CL_DEVICE_AVAILABLE , e_klt_opencl_property_is_cl_bool },
+  { "CL_DEVICE_COMPILER_AVAILABLE" , CL_DEVICE_COMPILER_AVAILABLE , e_klt_opencl_property_is_cl_bool },
+  { "CL_DEVICE_ENDIAN_LITTLE" , CL_DEVICE_ENDIAN_LITTLE , e_klt_opencl_property_is_cl_bool },
+  { "CL_DEVICE_ERROR_CORRECTION_SUPPORT" , CL_DEVICE_ERROR_CORRECTION_SUPPORT , e_klt_opencl_property_is_cl_bool },
+  { "CL_DEVICE_GLOBAL_MEM_CACHE_SIZE" , CL_DEVICE_GLOBAL_MEM_CACHE_SIZE , e_klt_opencl_property_is_cl_ulong },
+  { "CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE" , CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_GLOBAL_MEM_SIZE" , CL_DEVICE_GLOBAL_MEM_SIZE , e_klt_opencl_property_is_cl_ulong },
+  { "CL_DEVICE_IMAGE_SUPPORT" , CL_DEVICE_IMAGE_SUPPORT , e_klt_opencl_property_is_cl_bool },
+  { "CL_DEVICE_IMAGE2D_MAX_HEIGHT" , CL_DEVICE_IMAGE2D_MAX_HEIGHT , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_IMAGE2D_MAX_WIDTH" , CL_DEVICE_IMAGE2D_MAX_WIDTH , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_IMAGE3D_MAX_DEPTH" , CL_DEVICE_IMAGE3D_MAX_DEPTH , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_IMAGE3D_MAX_HEIGHT" , CL_DEVICE_IMAGE3D_MAX_HEIGHT , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_IMAGE3D_MAX_WIDTH" , CL_DEVICE_IMAGE3D_MAX_WIDTH , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_LOCAL_MEM_SIZE" , CL_DEVICE_LOCAL_MEM_SIZE , e_klt_opencl_property_is_cl_ulong },
+  { "CL_DEVICE_MAX_CLOCK_FREQUENCY" , CL_DEVICE_MAX_CLOCK_FREQUENCY , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MAX_COMPUTE_UNITS" , CL_DEVICE_MAX_COMPUTE_UNITS , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MAX_CONSTANT_ARGS" , CL_DEVICE_MAX_CONSTANT_ARGS , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE" , CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE , e_klt_opencl_property_is_cl_ulong },
+  { "CL_DEVICE_MAX_MEM_ALLOC_SIZE" , CL_DEVICE_MAX_MEM_ALLOC_SIZE , e_klt_opencl_property_is_cl_ulong },
+  { "CL_DEVICE_MAX_PARAMETER_SIZE" , CL_DEVICE_MAX_PARAMETER_SIZE , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_MAX_READ_IMAGE_ARGS" , CL_DEVICE_MAX_READ_IMAGE_ARGS , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MAX_SAMPLERS" , CL_DEVICE_MAX_SAMPLERS , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MAX_WORK_GROUP_SIZE" , CL_DEVICE_MAX_WORK_GROUP_SIZE , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS" , CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MAX_WORK_ITEM_SIZES" , CL_DEVICE_MAX_WORK_ITEM_SIZES , e_klt_opencl_property_is_array_size_t },
+  { "CL_DEVICE_MAX_WRITE_IMAGE_ARGS" , CL_DEVICE_MAX_WRITE_IMAGE_ARGS , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MEM_BASE_ADDR_ALIGN" , CL_DEVICE_MEM_BASE_ADDR_ALIGN , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_MIN_DATA_TYPE_ALIGN_SIZE" , CL_DEVICE_MIN_DATA_TYPE_ALIGN_SIZE , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_NAME" , CL_DEVICE_NAME , e_klt_opencl_property_is_array_char },
+  { "CL_DEVICE_PREFERRED_VECTOR_WIDTH_CHAR" , CL_DEVICE_PREFERRED_VECTOR_WIDTH_CHAR , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_PREFERRED_VECTOR_WIDTH_SHORT" , CL_DEVICE_PREFERRED_VECTOR_WIDTH_SHORT , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_PREFERRED_VECTOR_WIDTH_INT" , CL_DEVICE_PREFERRED_VECTOR_WIDTH_INT , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_PREFERRED_VECTOR_WIDTH_LONG" , CL_DEVICE_PREFERRED_VECTOR_WIDTH_LONG , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT" , CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE" , CL_DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_PROFILING_TIMER_RESOLUTION" , CL_DEVICE_PROFILING_TIMER_RESOLUTION , e_klt_opencl_property_is_size_t },
+  { "CL_DEVICE_VENDOR" , CL_DEVICE_VENDOR , e_klt_opencl_property_is_array_char },
+  { "CL_DEVICE_VENDOR_ID" , CL_DEVICE_VENDOR_ID , e_klt_opencl_property_is_cl_uint },
+  { "CL_DEVICE_VERSION" , CL_DEVICE_VERSION , e_klt_opencl_property_is_array_char },
+  { "CL_DRIVER_VERSION" , CL_DRIVER_VERSION , e_klt_opencl_property_is_array_char },
+  { "CL_DEVICE_LOCAL_MEM_TYPE" , CL_DEVICE_LOCAL_MEM_TYPE , e_klt_opencl_property_is_cl_device_local_mem_type },
+  { "CL_DEVICE_TYPE" , CL_DEVICE_TYPE , e_klt_opencl_property_is_cl_device_type },
+  { "CL_DEVICE_EXTENSIONS" , CL_DEVICE_EXTENSIONS , e_klt_opencl_property_is_array_char_split },
+  { "CL_DEVICE_PROFILE" , CL_DEVICE_PROFILE , e_klt_opencl_property_is_array_char },
+  { "CL_DEVICE_DOUBLE_FP_CONFIG" , CL_DEVICE_DOUBLE_FP_CONFIG , e_klt_opencl_property_is_cl_device_fp_config },
+  { "CL_DEVICE_EXECUTION_CAPABILITIES" , CL_DEVICE_EXECUTION_CAPABILITIES , e_klt_opencl_property_is_cl_device_exec_capabilities },
+  { "CL_DEVICE_GLOBAL_MEM_CACHE_TYPE" , CL_DEVICE_GLOBAL_MEM_CACHE_TYPE , e_klt_opencl_property_is_cl_device_mem_cache_type },
+  { "CL_DEVICE_QUEUE_PROPERTIES" , CL_DEVICE_QUEUE_PROPERTIES , e_klt_opencl_property_is_cl_command_queue_properties },
+  { "CL_DEVICE_SINGLE_FP_CONFIG" , CL_DEVICE_SINGLE_FP_CONFIG , e_klt_opencl_property_is_cl_device_fp_config },
+  { "CL_DEVICE_HALF_FP_CONFIG" , 0 & CL_DEVICE_HALF_FP_CONFIG , e_klt_opencl_property_is_cl_device_fp_config }
+};
+
+void klt_opencl_property_to_JSON(struct klt_opencl_property_t * property, cl_device_id device, const char * indent) {
+  if (property->property == 0) {
+    printf("\"\"");
+    return;
+  }
+
+  size_t i;
+  cl_int status;
+  size_t size;
+
+  status = clGetDeviceInfo(device, property->property, 0, NULL, &size);
+  klt_check_opencl_status("[Error] clGetDeviceInfo (size) returns:", status);
+
+  void * value = malloc(size);
+  assert(value != NULL);
+
+  status = clGetDeviceInfo(device, property->property, size, value, NULL);
+  klt_check_opencl_status("[Error] clGetDeviceInfo (value) returns:", status);
+
+  switch (property->type) {
+    case e_klt_opencl_property_is_cl_bool:    printf("%u"    , *(cl_bool * )value); break;
+    case e_klt_opencl_property_is_cl_uint:    printf("%u"    , *(cl_uint * )value); break;
+    case e_klt_opencl_property_is_cl_ulong:   printf("%lu"   , *(cl_ulong *)value); break;
+    case e_klt_opencl_property_is_size_t:     printf("%zd"   , *(size_t *  )value); break;
+    case e_klt_opencl_property_is_array_char: printf("\"%s\"",  (char *    )value); break;
+    case e_klt_opencl_property_is_array_size_t:
+    {
+      printf("[");
+      for (i = 0; i < size / sizeof(size_t); i++) {
+        if (i > 0) printf(", ");
+        printf("%zd", ((size_t *)value)[i]);
+      }
+      printf("]");
+      break;
+    }
+    case e_klt_opencl_property_is_array_char_split:
+    {
+      char * values = (char *)value;
+      char * tok = strtok(values, " ");
+      printf("[ ");
+      if (tok != NULL) {
+        printf("\n%s  \"%s\"", indent, tok);
+        tok = strtok(NULL, " ");
+        while (tok != NULL) {
+          printf(",\n%s  \"%s\"", indent, tok);
+          tok = strtok(NULL, " ");
+        }
+      }
+      printf("\n%s]", indent);
+      break;
+    }
+    case e_klt_opencl_property_is_cl_device_type:
+    {
+      cl_device_type device_type = *(cl_device_type *)value;
+      printf("\"");
+      if ((device_type & CL_DEVICE_TYPE_CPU        ) != 0) printf("CPU ");
+      if ((device_type & CL_DEVICE_TYPE_GPU        ) != 0) printf("GPU ");
+      if ((device_type & CL_DEVICE_TYPE_ACCELERATOR) != 0) printf("accelerator ");
+      if ((device_type & CL_DEVICE_TYPE_DEFAULT    ) != 0) printf("default ");
+      printf("\"");
+      break;
+    }
+    case e_klt_opencl_property_is_cl_device_local_mem_type:
+    {
+      cl_device_local_mem_type device_local_mem_type = *(cl_device_local_mem_type *)value;
+      switch (device_local_mem_type) {
+        case CL_LOCAL:  printf("\"local\""); break;
+        case CL_GLOBAL: printf("\"global\""); break;
+        default:
+          assert(0);
+      }
+      break;
+    }
+    case e_klt_opencl_property_is_cl_device_mem_cache_type:
+    { //CL_NONE, CL_READ_ONLY_CACHE, and CL_READ_WRITE_CACHE.
+      cl_device_mem_cache_type device_mem_cache_type = *(cl_device_mem_cache_type *)value;
+      switch (device_mem_cache_type) {
+        case CL_NONE:             printf("\"no\""); break;
+        case CL_READ_ONLY_CACHE:  printf("\"ro\""); break;
+        case CL_READ_WRITE_CACHE: printf("\"rw\""); break;
+        default:
+          assert(0);
+      }
+      break;
+    }
+    case e_klt_opencl_property_is_cl_device_exec_capabilities:
+    {
+      cl_device_exec_capabilities device_exec_capabilities = *(cl_device_exec_capabilities *)value;
+      printf("\"");
+      if ((device_exec_capabilities & CL_EXEC_KERNEL       ) != 0) printf("kernel ");
+      if ((device_exec_capabilities & CL_EXEC_NATIVE_KERNEL) != 0) printf("native ");
+      printf("\"");
+      break;
+    }
+    case e_klt_opencl_property_is_cl_command_queue_properties:
+    {
+      cl_command_queue_properties command_queue_properties = *(cl_command_queue_properties *)value;
+      printf("\"");
+      if ((command_queue_properties & CL_QUEUE_PROFILING_ENABLE             ) != 0) printf("profiling ");
+      if ((command_queue_properties & CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE) != 0) printf("out-of-order ");
+      printf("\"");
+      break;
+    }
+    case e_klt_opencl_property_is_cl_device_fp_config:
+    {
+      cl_device_fp_config device_fp_config = *(cl_device_fp_config *)value;
+      printf("\"");
+      if ((device_fp_config & CL_FP_DENORM          ) != 0) printf("CL_FP_DENORM ");
+      if ((device_fp_config & CL_FP_INF_NAN         ) != 0) printf("CL_FP_INF_NAN ");
+      if ((device_fp_config & CL_FP_ROUND_TO_NEAREST) != 0) printf("CL_FP_ROUND_TO_NEAREST ");
+      if ((device_fp_config & CL_FP_ROUND_TO_ZERO   ) != 0) printf("CL_FP_ROUND_TO_ZERO ");
+      if ((device_fp_config & CL_FP_ROUND_TO_INF    ) != 0) printf("CL_FP_ROUND_TO_INF ");
+//    if ((device_fp_config & CP_FP_FMA             ) != 0) printf("CP_FP_FMA ");
+      printf("\"");
+      break;
+    }
+    default:
+      assert(0);
+  }
+
+  free(value);
+}
+
 void klt_opencl_device_to_JSON(cl_platform_id platform, cl_device_id device, const char * indent) {
-  printf("{\n");
-  printf("%s  TODO", indent);
+  char * new_indent = malloc(strlen(indent) + 3);
+  memset(new_indent, 0, strlen(indent) + 3);
+  strcat(new_indent, indent);
+  strcat(new_indent, "  ");
+  printf("{\n%s", new_indent);
+  size_t i;
+  for (i = 0; i < KLT_OPENCL_NUM_PROPERTIES; i++) {
+    if (i > 0) printf(",\n%s", new_indent);
+    printf("\"%s\":", klt_opencl_property[i].name);
+    klt_opencl_property_to_JSON(&(klt_opencl_property[i]), device, new_indent);
+  }
+  free(new_indent);
   printf("\n%s}", indent);
 }
 
