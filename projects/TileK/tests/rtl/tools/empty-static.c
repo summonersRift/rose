995f4842233e1de3e8b22a44c8f21f7ef10d769e
Tristan Vanderbruggen <vanderbruggentristan@gmail.com>
2015-10-23 11:19:02 -0400

    [TileK] Fixed LD paths.

--- projects/TileK/tests/rtl/tools/empty-static.c
+++ projects/TileK/tests/rtl/tools/empty-static.c
@@ -5,7 +5,7 @@ char * opencl_kernel_file = NULL;
 char * opencl_kernel_options = NULL;
 char * opencl_klt_runtime_lib = NULL;
 
-char * klt_file_stem = NULL;
+char * klt_file_stem = "";
 size_t klt_num_kernels = 0;
 struct klt_kernel_desc_t klt_kernel_desc[0];
 
